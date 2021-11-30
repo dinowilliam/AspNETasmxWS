1154bc283f8a0a852dc71d2452f09b061fe54e4a
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2021-11-30 20:05:29 -0300

    Add project files.

---
+++ AspNETasmxWS/MediaWS.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="MediaWS.asmx.cs" Class="AspNETasmxWS.MediaWS" %>
---
+++ AspNETasmxWS/MediaWS.asmx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace AspNETasmxWS {
+    /// <summary>
+    /// Summary description for DocWS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MediaWS : System.Web.Services.WebService  {
+
+        [WebMethod]
+        public string SaveMedia()
+        {
+            return "Hello World";
+        }
+
+
+        [WebMethod]
+        public string GetMedia()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string DeleteMedia()
+        {
+            return "Hello World";
+        }
+    }
+}
