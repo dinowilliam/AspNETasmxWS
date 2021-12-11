717ad587df981ca40c4fea9767645771e0e3f333
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2021-12-11 20:07:34 -0300

    Add first test MediaInsertCommand_WhenExecuteInsert_IsFailed

--- AspNETasmxWS.Test/MediaInsertCommandTest.cs
+++ AspNETasmxWS.Test/MediaInsertCommandTest.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AspNETasmxWS.Test {
+
+    using AspNETasmxWS.Commands;
+    using AspNETasmxWS.Entities;
+    using System.Collections.Generic;
+
     [TestClass]
     public class MediaInsertCommandTest {
+
+        public MediaInsertCommand mediaInsertCommand;
+        public Requisition requisitionMock;
+
+        [TestInitialize()]
+        public void Initialize() {
+
+            mediaInsertCommand = new MediaInsertCommand();
+            requisitionMock = A.Fake<Requisition>();
+        }
+
         [TestMethod]
-        public void TestMethod1()
-        {
+        public void MediaInsertCommand_WhenExecuteInsert_IsFailed() {
+
+            //Arrange                       
+
+            //Act
+            var assured = mediaInsertCommand.Execute(requisitionMock);
+
+            //Assure
+            Assert.IsFalse(assured);
+
+        }
+
+        [TestCleanup()]
+        public void Cleanup() {
+            mediaInsertCommand = null;
+            requisitionMock = null;
         }
     }
 }
